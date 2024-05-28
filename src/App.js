ca2bfc429705945cd66765a9cac94390056ee7e7
Kailyan <kailyan.theresine@gmail.com>
2024-05-28 15:00:14 +0200

    Soutenance

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
-import { Route, Routes,} from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { Route, Routes, Navigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Home from './pages/Home/Home.jsx';
 import Login from './pages/Login/Login.jsx';
+import Error from './pages/Error/Error.jsx';
 import Profile from './pages/Profile/Profile.jsx';
 import { loginSuccess } from '../src/redux/actions/auth.actions.jsx';
 import './sass/_Main.scss';
 
 export default function App () {
     const dispatch = useDispatch();
+    const isConnected = useSelector((state) => state.auth.isConnected);
 
     useEffect(() => {
       const token = window.sessionStorage.getItem('token') || window.localStorage.getItem('token');
@@ -18,7 +20,8 @@ export default function App () {
         dispatch(loginSuccess(token));
       }
     }, [dispatch]);
-  
+    
+
     return (
         <div>
             <Header />
@@ -27,8 +30,9 @@ export default function App () {
                 <Route path='login' element={<Login />} />
                 <Route 
                     path='profile' 
-                    element={<Profile />} 
+                    element={isConnected ? <Profile /> : <Navigate to="/login" />} 
                 />
+                <Route path='*' element={<Error />} />
             </Routes>
             <Footer />
         </div>
